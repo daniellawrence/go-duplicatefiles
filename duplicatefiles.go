24e9af99604b7e5e40967262950880e5a06967f0
Daniel Lawrence <dannyla@linux.com>
2014-06-18 22:31:26 +1000

    Added a wasted space number to the output

--- duplicatefiles.go
+++ duplicatefiles.go
@@ -1,20 +1,19 @@
 package main
- 
+
 import (
-   "path/filepath"
-   "fmt"
-   "os"
-   "strings"
-   "crypto/sha256"
-   "io/ioutil"
-   "encoding/hex"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
- 
-func main(){
+
+func main() {
 
 	args := os.Args[1:]
 	location := "."
-	fmt.Println(args)
 	if len(args) > 0 {
 		location = args[0]
 	}
@@ -23,28 +22,34 @@ func main(){
 }
 
 func checkDuplicates(files []string) (result bool) {
-     stored_hashs  := make(map[string][]string)
-     for _, filename := range files {
-	 hasher := sha256.New()
-	 file_content, _ := ioutil.ReadFile(filename)
-	 hasher.Write(file_content)
-	 file_hash := hex.EncodeToString(hasher.Sum(nil))
-	     
-	 // If the file_hash is already in the stored_hashs
-	 // then we know we have a duplicate
-	 if _, ok := stored_hashs[file_hash]; ok {
-	    fmt.Printf("%s is a Duplicate of %s\n", filename, stored_hashs[file_hash])
-	 }
-	 stored_hashs[file_hash] = append(stored_hashs[file_hash], filename)
-     }
-     return true
+	stored_hashs := make(map[string][]string)
+	wasted_space := 0
+
+	for _, filename := range files {
+		hasher := sha256.New()
+		file_content, _ := ioutil.ReadFile(filename)
+		hasher.Write(file_content)
+		file_hash := hex.EncodeToString(hasher.Sum(nil))
+
+		// If the file_hash is already in the stored_hashs
+		// then we know we have a duplicate
+		if _, ok := stored_hashs[file_hash]; ok {
+			fmt.Printf("%s is a Duplicate of %s\n", filename, stored_hashs[file_hash])
+			wasted_space = len(stored_hashs[file_hash]) * len(file_content)
+		}
+		stored_hashs[file_hash] = append(stored_hashs[file_hash], filename)
+	}
+	if wasted_space > 0 {
+		fmt.Println(files[0], "Wasted space:", wasted_space)
+	}
+	return true
 
 }
- 
-func GoWalk(location string) {
-	dict  := make(map[int64][]string)
 
-	filepath.Walk(location, func(path string, fileinfo os.FileInfo, _ error)(err error){
+func GoWalk(location string) {
+	dict := make(map[int64][]string)
+
+	filepath.Walk(location, func(path string, fileinfo os.FileInfo, _ error) (err error) {
 		// skip over hidden files
 		if strings.Contains(path, "/.") {
 			return
@@ -53,7 +58,10 @@ func GoWalk(location string) {
 		if strings.HasSuffix(path, "~") {
 			return
 		}
-		fmt.Println(path)
+		// Duplicate dirs... you mean emtpy dirs
+		if fileinfo.IsDir() {
+			return
+		}
 		file_size := fileinfo.Size()
 		dict[file_size] = append(dict[file_size], path)
 		return
