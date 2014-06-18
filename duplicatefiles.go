99ab0a82e903741da3cb8d12ae6fc09970a947d5
Daniel Lawrence <dannyla@linux.com>
2014-06-18 21:09:21 +1000

    allow for command-line args & better hidden files

--- duplicatefiles.go
+++ duplicatefiles.go
@@ -11,9 +11,15 @@ import (
 )
  
 func main(){
-     location := "."
-     GoWalk(location)
-     return
+
+	args := os.Args[1:]
+	location := "."
+	fmt.Println(args)
+	if len(args) > 0 {
+		location = args[0]
+	}
+	GoWalk(location)
+	return
 }
 
 func checkDuplicates(files []string) (result bool) {
@@ -23,6 +29,9 @@ func checkDuplicates(files []string) (result bool) {
 	 file_content, _ := ioutil.ReadFile(filename)
 	 hasher.Write(file_content)
 	 file_hash := hex.EncodeToString(hasher.Sum(nil))
+	     
+	 // If the file_hash is already in the stored_hashs
+	 // then we know we have a duplicate
 	 if _, ok := stored_hashs[file_hash]; ok {
 	    fmt.Printf("%s is a Duplicate of %s\n", filename, stored_hashs[file_hash])
 	 }
@@ -37,13 +46,14 @@ func GoWalk(location string) {
 
 	filepath.Walk(location, func(path string, fileinfo os.FileInfo, _ error)(err error){
 		// skip over hidden files
-		if strings.HasPrefix(path, ".") {
+		if strings.Contains(path, "/.") {
 			return
 		}
 		// Skip over tmp files
 		if strings.HasSuffix(path, "~") {
 			return
 		}
+		fmt.Println(path)
 		file_size := fileinfo.Size()
 		dict[file_size] = append(dict[file_size], path)
 		return
